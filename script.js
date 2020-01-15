3d6a652a5fa4e5628b165031204aa87b869c13a5
Miguel <migueluzcategui09@gmail.com>
2020-01-14 21:45:46 -0400

    Basic UI added

--- script.js
+++ script.js
@@ -1,43 +1,82 @@
 // Variables
+const general = document.querySelector('#general')
+const buttons = document.querySelector('#buttons')
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const reset = document.querySelector('.reset')
+
 let playerScore = 0;
 let computerScore = 0;
 let vs;
 
-// Asks the player for his/her selection
-function ask() {
-        let choice = prompt("Rock, paper or scissors?").toLocaleLowerCase()
-        return(choice)
-    }
+let div = document.createElement('div');
+    div.classList.add('results')
 
-    // Randomly gives computer's selection
+let competition = document.createElement('p');
+    competition.classList.add('vs')
+
+let resultsPara = document.createElement('p');
+    resultsPara.classList.add('round')
+
+let win = document.createElement('p');
+    win.classList.add('win');
+
+let winner = document.createElement('p');
+    winner.classList.add('winner');
+
+rock.addEventListener('click', function () {
+    dataLog()
+});
+
+paper.addEventListener('click', function () {
+    dataLog()
+});
+
+scissors.addEventListener('click', function () {
+    dataLog()
+});
+
+reset.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    
+    competition.textContent = "";
+    resultsPara.textContent = "";
+    win.textContent = "";
+    winner.textContent = "";
+})
+
+// Randomly gives computer's selection
 function computerPlay() { 
-        let select = ["rock", "paper", "scissors"]
+        let select = ["Rock", "Paper", "Scissors"]
         let random = select[Math.floor(Math.random() * 3)];
         return random
     } 
 
-    // Plays a single round and gives the round winner
+// Plays a single round and gives the round winner
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors" ||
-        playerSelection == "scissors" && computerSelection == "paper" ||
-        playerSelection == "paper" && computerSelection == "rock"){
-            vs = "You won, " + playerSelection + " beats " + computerSelection
+    if (playerSelection == "Rock" && computerSelection == "Scissors" ||
+        playerSelection == "Scissors" && computerSelection == "Paper" ||
+        playerSelection == "Paper" && computerSelection == "Rock"){
+            vs = "You won. " + playerSelection + " beats " + computerSelection
         
     } else if (playerSelection == computerSelection) {
             vs = "That's a draw"
          
     } else {
-            vs = "You lose, " + computerSelection + " beats " + playerSelection
+            vs = "You lose " + computerSelection + " beats " + playerSelection
     }
     
-    console.log(playerSelection + " vs " + computerSelection)
-    return(vs);
+    competition.textContent = playerSelection + " vs " + computerSelection;
+    return( vs);
 }
+
 // Records both scores
 function score() {
-    if (vs == "You won, rock beats scissors" || 
-        vs == "You won, scissors beats paper" || 
-        vs == "You won, paper beats rock") {
+    if (vs == "You won. Rock beats Scissors" || 
+        vs == "You won. Scissors beats Paper" || 
+        vs == "You won. Paper beats Rock") {
             playerScore += 1;
     
     } else if (vs == "That's a draw") {
@@ -48,40 +87,33 @@ function score() {
         computerScore += 1;
     }
 
-console.log("Player wins: " + playerScore + 
-" | Cpu wins: " + computerScore)
+    return("Player wins: " + playerScore + 
+            " | Cpu wins: " + computerScore)
 }
 // Gives a winner according to scores
-function winner() {
-    if (computerScore > playerScore) {
-       return("The match is over. Cpu won this match!")
-   } else if (playerScore > computerScore) {
-       return("The match is over. You won this match!")
-   } else {
-       return("The match is over. We have no winner today")
-   }
+function matchWinner() {
+    if (playerScore == 5 || computerScore == 5) {
+        if (computerScore > playerScore) {
+            return("The match is over. Cpu won this match!")
+        } else if (playerScore > computerScore) {
+            return("The match is over. You won this match!")
+        } else {
+            return("The match is over. We have no winner today")
+        }
+}
 }
 
-// Plays 5 rounds and gives the match winner at the end
-function game() {
-   
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(ask(), computerPlay()));
-        score()
-    }
-    
-   return(winner())
+//Logs all the results
+function dataLog() {
+    if (playerScore < 5 && computerScore < 5) {
+        resultsPara.textContent = playRound("Rock", computerPlay());
+        win.textContent = score();
+        winner.textContent = matchWinner();
+    }    
 }
-//Restarts the game
-function reset() {
-    let again = prompt("Wanna play again?").toLocaleLowerCase();
-    if (again == "yes") {
-        playerScore = 0;
-        computerScore = 0;
-        return(game())
-    } else {
-        return("Alright then. Bye")
-    }
-}
-console.log(game())
-console.log(reset())
+
+div.appendChild(competition);
+div.appendChild(resultsPara);
+div.appendChild(win);
+div.appendChild(winner);
+general.appendChild(div);
