02e71ec47957148a3a0d6179421c7071c9ee33b8
Miguel <migueluzcategui09@gmail.com>
2020-01-20 12:37:16 -0400

    UI improved, game mechanics fixed, animations added. Game is ready

--- script.js
+++ script.js
@@ -1,71 +1,109 @@
 // Variables
-const general = document.querySelector('#general')
-const buttons = document.querySelector('#buttons')
-const results = document.querySelector('#results')
+const general = document.querySelector('#general');
+const buttons = document.querySelector('#buttons');
+const results = document.querySelector('#results');
+const start = document.querySelector('#start');
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
-const reset = document.querySelector('.reset')
+const reset = document.querySelector('#reset');
+const cpuRock = document.querySelector('.cpuRock');
 
 let playerScore = 0;
 let computerScore = 0;
 let vs;
 
-let competition = document.createElement('p');
-    competition.classList.add('vs')
-    competition.textContent = "Rules are simple:"
+let playerScoreBoard = document.createElement('p');
+playerScoreBoard.classList.add('playerScore');
+results.appendChild(playerScoreBoard);
 
-let resultsPara = document.createElement('p');
-    resultsPara.classList.add('round')
-    resultsPara.textContent = "1) Rock beats scissors"
+let cpuScoreBoard = document.createElement('p');
+cpuScoreBoard.classList.add('cpuScore');
+results.appendChild(cpuScoreBoard);
 
-let win = document.createElement('p');
-    win.classList.add('win');
-    win.textContent = "2) Scissors beat paper"
-
-let winner = document.createElement('p');
-    winner.classList.add('winner');
-    winner.textContent = "3) Paper beats rock"
+start.addEventListener('click', function () {
+    startGame();
+});
 
 rock.addEventListener('click', function () {
     if (playerScore < 5 && computerScore < 5) {
-        resultsPara.textContent = playRound("Rock", computerPlay());
-        win.textContent = score();
-        winner.textContent = matchWinner();
+        
+        playRound('Rock', computerPlay());
+        score();
+        matchWinner();      
     }
 });
 
 paper.addEventListener('click', function () {
     if (playerScore < 5 && computerScore < 5) {
-        resultsPara.textContent = playRound("Paper", computerPlay());
-        win.textContent = score();
-        winner.textContent = matchWinner();
+        
+        playRound('Paper', computerPlay());
+        score();
+        matchWinner();
     }
 });
 
 scissors.addEventListener('click', function () {
     if (playerScore < 5 && computerScore < 5) {
-        resultsPara.textContent = playRound("Scissors", computerPlay());
-        win.textContent = score();
-        winner.textContent = matchWinner();
-    }
+        
+        playRound('Scissors', computerPlay());
+        score();
+        matchWinner();
+    }   
 });
 
 reset.addEventListener('click', () => {
-    playerScore = 0;
-    computerScore = 0;
     
-    competition.textContent = "Rules are simple:"
-    resultsPara.textContent = "1) Rock beats scissors";
-    win.textContent = "2) Scissors beat paper";
-    winner.textContent = "3) Paper beats rock";
+    playerScore = 0;
+    computerScore = 0;  
+
+    playerScoreBoard.textContent = "Your wins: " + playerScore;
+    cpuScoreBoard.textContent = "Cpu wins: " + computerScore;
+
+    document.getElementById('title').textContent = "Can you beat the computer?"
+    document.getElementById('reset').style.visibility = "hidden";
+    document.getElementById("cpuRock").style.scale = 1;
+    document.getElementById("cpuPaper").style.scale = 1;
+    document.getElementById("cpuScissors").style.scale = 1;
+    document.getElementById('buttons').style.scale = 1;
+
 })
+
+function startGame() {
+    
+    document.getElementById("buttons").style.visibility = "visible";
+    document.getElementById("border").style.visibility = "visible";
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("rules").style.visibility = "hidden";
+    
+    document.getElementById("title").textContent = "Can you beat the computer?"
+    
+    playerScoreBoard.textContent = "Your wins: " + playerScore;
+    cpuScoreBoard.textContent = "Cpu wins: " + computerScore;
+    
+}
 
 // Randomly gives computer's selection
 function computerPlay() { 
         let select = ["Rock", "Paper", "Scissors"]
         let random = select[Math.floor(Math.random() * 3)];
-        return random
+
+        if (random == "Rock") {
+            document.getElementById("cpuRock").style.scale = 1000;
+            document.getElementById("cpuPaper").style.scale = 1;
+            document.getElementById("cpuScissors").style.scale = 1;
+            return(random)
+        } else if (random == "Paper") {
+            document.getElementById("cpuRock").style.scale = 1;
+            document.getElementById("cpuPaper").style.scale = 1000;
+            document.getElementById("cpuScissors").style.scale = 1;
+            return(random)
+        } else if (random == "Scissors") {
+            document.getElementById("cpuRock").style.scale = 1;
+            document.getElementById("cpuPaper").style.scale = 1;
+            document.getElementById("cpuScissors").style.scale = 1000;
+            return(random)
+        }
     } 
 
 // Plays a single round and gives the round winner
@@ -82,7 +120,6 @@ function playRound(playerSelection, computerSelection) {
             vs = "You lose " + computerSelection + " beats " + playerSelection
     }
     
-    competition.textContent = playerSelection + " vs " + computerSelection;
     return( vs);
 }
 
@@ -101,23 +138,20 @@ function score() {
         computerScore += 1;
     }
 
-    return("Player wins: " + playerScore + 
-            " | Cpu wins: " + computerScore)
+    playerScoreBoard.textContent = "Your wins: " + playerScore;
+    cpuScoreBoard.textContent = "Cpu wins: " + computerScore;
+
 }
 // Gives a winner according to scores
 function matchWinner() {
     if (playerScore == 5 || computerScore == 5) {
         if (computerScore > playerScore) {
-            return("The match is over. Cpu won this match!")
+            
+            document.getElementById('title').textContent = "The match is over. Cpu won this match!";
         } else if (playerScore > computerScore) {
-            return("The match is over. You won this match!")
-        } else {
-            return("The match is over. We have no winner today")
-        }
+            document.getElementById('title').textContent = "The match is over. You won this match!"
+        } 
+        document.getElementById('reset').style.visibility = "visible";
+        document.getElementById('buttons').style.scale = 0.001;
+    }
 }
-}
-
-results.appendChild(competition);
-results.appendChild(resultsPara);
-results.appendChild(win);
-results.appendChild(winner);
